42ed7d10acc29c27c19ec0fc64261e9d779ee08b
blessme333 <blessme333@gmail.com>
2019-12-08 11:31:37 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20191208_02_intent04_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.a20191208_02_intent04_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
